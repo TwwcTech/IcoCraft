49d93ce6970ce1be51826ae19c6a88c39a841f06
twwct <twwc.tech@gmail.com>
2023-07-23 18:26:34 -0600

    Singleton instance created for the Icon Converter Tool

---
+++ backend/Singletons/IconConverterTool.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IcoCraft.backend.Singletons
+{
+    internal class IconConverterTool
+    {
+        private readonly IconConverterTool? _instance;
+        object instanceLock = new();
+
+        private IconConverterTool () { }
+
+        public IconConverterTool Instance
+        {
+            get
+            {
+                lock (instanceLock)
+                {
+                    if (_instance == null)
+                    {
+                        return new IconConverterTool();
+                    }
+                    return _instance;
+                }
+            }
+        }
+    }
+}

55e6d40ff55bdd537d38f1e9baa16e62a9c1dae6
twwct <twwc.tech@gmail.com>
2023-07-23 21:52:42 -0600

    File created

---
+++ frontend/Windows/CraftedFilesWindow.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IcoCraft.frontend.Windows
+{
+    public partial class CraftedFilesWindow : Form
+    {
+        public CraftedFilesWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}

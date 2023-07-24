36a5f16526a9fce52e4d6f6138398943336d90b9
twwct <twwc.tech@gmail.com>
2023-07-23 19:37:08 -0600

    Get methods complete; not yet tested

--- backend/EventHandlers/Publishers/CraftController.cs
+++ backend/EventHandlers/Publishers/CraftController.cs
@@ -8,5 +8,29 @@ namespace IcoCraft.backend.EventHandlers.Publishers
 {
     internal class CraftController
     {
+        // Add events
+
+        public string GetPngPath(string pngPath)
+        {
+            if (pngPath == string.Empty)
+            {
+                return string.Empty;
+            }
+            return pngPath;
+        }
+
+        public string GetCraftedFileName(string fileName)
+        {
+            if (fileName == string.Empty)
+            {
+                return string.Empty;
+            }
+            return fileName;
+        }
+
+        public void Craft(string pngPath, string fileName)
+        {
+            // Invoke delegate here
+        }
     }
 }

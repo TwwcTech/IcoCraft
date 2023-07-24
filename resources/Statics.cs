e38b83cd5a43d548974c2f4327b94245a16712cd
twwct <twwc.tech@gmail.com>
2023-07-23 22:17:00 -0600

    variables added

--- resources/Statics.cs
+++ resources/Statics.cs
@@ -8,5 +8,8 @@ namespace IcoCraft.resources
 {
     internal class Statics
     {
+        public static DateTime DateTimeStamp = DateTime.Now;
+        public static string CurrentUser = ""; // Complete the string
+        public const string CraftedIconDest = ""; // Complete the string
     }
 }

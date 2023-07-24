3111729de83b64a4ce6001fd8f05a075aa88d4a1
twwct <twwc.tech@gmail.com>
2023-07-23 19:37:28 -0600

    skeleton added

--- backend/EventHandlers/Subscribers/CraftOperator.cs
+++ backend/EventHandlers/Subscribers/CraftOperator.cs
@@ -8,5 +8,9 @@ namespace IcoCraft.backend.EventHandlers.Subscribers
 {
     internal class CraftOperator
     {
+        public void OnCraft()
+        {
+            // Code goes here
+        }
     }
 }

40c60875431acb24fd1a74389bd3261b0da43b49
twwct <twwc.tech@gmail.com>
2023-07-24 22:32:09 -0600

    fields and params added

--- resources/Statics.cs
+++ resources/Statics.cs
@@ -1,9 +1,34 @@
-﻿namespace IcoCraft.resources
+﻿using System.Security.Principal;
+
+namespace IcoCraft.resources
 {
     internal class Statics
     {
-        public static DateTime DateTimeStamp = DateTime.Now;
-        public static string CurrentUser = ""; // Complete the string
-        public const string CraftedIconDest = @"C:\Users\twwct\OneDrive\Desktop\"; // Testing
+        private readonly static DateTime _dateTimeStamp = DateTime.Now;
+        private readonly static string _currentUser = WindowsIdentity.GetCurrent().Name;
+        private static string? _primaryDestDirPath;
+        private static string? _altDestDirPath;
+
+        public static DateTime DateTimeStamp
+        {
+            get { return _dateTimeStamp; }
+        }
+
+        public static string CurrentUser
+        {
+            get { return _currentUser; }
+        }
+
+        public static string PrimaryDestDirPath
+        {
+            get { return _primaryDestDirPath!; }
+            private set { _primaryDestDirPath = $@"C:\Users\{CurrentUser}\OneDrive\Documents\"; }
+        }
+
+        public static string AltDestDirPath
+        {
+            get { return _altDestDirPath!; }
+            private set { _altDestDirPath = $@"C:\{CurrentUser}\Documents\"; }
+        }
     }
 }

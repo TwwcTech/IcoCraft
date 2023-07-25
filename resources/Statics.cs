c7f9851a59e6898d1e6e59de593a3c3ccbf9b8b4
twwct <twwc.tech@gmail.com>
2023-07-24 23:40:33 -0600

    removed correct path field and param

--- resources/Statics.cs
+++ resources/Statics.cs
@@ -1,13 +1,11 @@
-﻿using System.Security.Principal;
-
-namespace IcoCraft.resources
+﻿namespace IcoCraft.resources
 {
     internal class Statics
     {
-        private readonly static DateTime _dateTimeStamp = DateTime.Now;
-        private readonly static string _currentUser = WindowsIdentity.GetCurrent().Name;
-        private static string? _primaryDestDirPath;
-        private static string? _altDestDirPath;
+        private static DateTime _dateTimeStamp = DateTime.Now;
+        private static string _currentUser = Environment.UserName;
+        private static string? _primaryDestDirPath = $@"C:\Users\{CurrentUser}\OneDrive\Documents\";
+        private static string? _altDestDirPath = $@"C:\{CurrentUser}\Documents\";
 
         public static DateTime DateTimeStamp
         {
@@ -22,13 +20,11 @@ namespace IcoCraft.resources
         public static string PrimaryDestDirPath
         {
             get { return _primaryDestDirPath!; }
-            private set { _primaryDestDirPath = $@"C:\Users\{CurrentUser}\OneDrive\Documents\"; }
         }
 
         public static string AltDestDirPath
         {
             get { return _altDestDirPath!; }
-            private set { _altDestDirPath = $@"C:\{CurrentUser}\Documents\"; }
         }
     }
 }

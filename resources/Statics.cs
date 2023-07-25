bc8d7241414a8ed8f220423c350e3b599088cc45
twwct <twwc.tech@gmail.com>
2023-07-24 19:13:28 -0600

    test string added

--- resources/Statics.cs
+++ resources/Statics.cs
@@ -4,6 +4,6 @@
     {
         public static DateTime DateTimeStamp = DateTime.Now;
         public static string CurrentUser = ""; // Complete the string
-        public const string CraftedIconDest = ""; // Complete the string
+        public const string CraftedIconDest = @"C:\Users\twwct\OneDrive\Desktop\"; // Testing
     }
 }

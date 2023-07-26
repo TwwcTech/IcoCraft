3608be783d6781e9a4086d2167155a7c4617570f
twwct <twwc.tech@gmail.com>
2023-07-25 21:38:57 -0600

    comment removed

--- backend/Singletons/IconConverterTool.cs
+++ backend/Singletons/IconConverterTool.cs
@@ -46,7 +46,6 @@
                 }
                 catch (Exception ex)
                 {
-                    // Testing
                     throw new Exception(ex.ToString());
                 }
 

7b62ead761b935e4b02115d3fab9020ff16280db
twwct <twwc.tech@gmail.com>
2023-07-29 23:56:01 -0600

    DataGrid view added and source binded

--- frontend/Windows/CraftedFilesWindow.cs
+++ frontend/Windows/CraftedFilesWindow.cs
@@ -1,4 +1,6 @@
-﻿namespace IcoCraft.frontend.Windows
+﻿using IcoCraft.backend.Singletons;
+
+namespace IcoCraft.frontend.Windows
 {
     public partial class CraftedFilesWindow : Form
     {
@@ -9,7 +11,11 @@
 
         private void CraftedFilesWindow_Load(object sender, EventArgs e)
         {
-
+            string savePath = DestDirManager.Instance.GetCorrectPath();
+            FileInfo[] craftedIcons = DestDirManager.Instance.GetIcons($@"{savePath}CraftedIcons");
+            CraftedIconsDataGrid.DataSource = craftedIcons;
+            CraftedIconsDataGrid.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.DisplayedCells;
+            CraftedIconsDataGrid.Refresh();
         }
     }
 }

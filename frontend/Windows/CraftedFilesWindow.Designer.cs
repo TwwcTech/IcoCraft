9ad2f2d70e286ae51d15021d02da127d4984c267
twwct <twwc.tech@gmail.com>
2023-07-29 23:56:16 -0600

    GUI edit

--- frontend/Windows/CraftedFilesWindow.Designer.cs
+++ frontend/Windows/CraftedFilesWindow.Designer.cs
@@ -28,24 +28,48 @@
         /// </summary>
         private void InitializeComponent()
         {
+            components = new System.ComponentModel.Container();
             CraftedControlPanel = new Panel();
+            destDirManagerBindingSource = new BindingSource(components);
+            CraftedIconsDataGrid = new DataGridView();
+            ((System.ComponentModel.ISupportInitialize)destDirManagerBindingSource).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)CraftedIconsDataGrid).BeginInit();
             SuspendLayout();
             // 
             // CraftedControlPanel
             // 
             CraftedControlPanel.BackColor = Color.FromArgb(100, 151, 177);
             CraftedControlPanel.Dock = DockStyle.Bottom;
-            CraftedControlPanel.Location = new Point(0, 619);
+            CraftedControlPanel.Location = new Point(0, 728);
             CraftedControlPanel.Name = "CraftedControlPanel";
-            CraftedControlPanel.Size = new Size(1120, 160);
+            CraftedControlPanel.Size = new Size(1490, 160);
             CraftedControlPanel.TabIndex = 0;
+            // 
+            // destDirManagerBindingSource
+            // 
+            destDirManagerBindingSource.DataSource = typeof(backend.Singletons.DestDirManager);
+            // 
+            // CraftedIconsDataGrid
+            // 
+            CraftedIconsDataGrid.AllowUserToAddRows = false;
+            CraftedIconsDataGrid.AllowUserToDeleteRows = false;
+            CraftedIconsDataGrid.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            CraftedIconsDataGrid.Dock = DockStyle.Fill;
+            CraftedIconsDataGrid.Location = new Point(0, 0);
+            CraftedIconsDataGrid.Name = "CraftedIconsDataGrid";
+            CraftedIconsDataGrid.ReadOnly = true;
+            CraftedIconsDataGrid.RowHeadersWidth = 62;
+            CraftedIconsDataGrid.RowTemplate.Height = 33;
+            CraftedIconsDataGrid.Size = new Size(1490, 728);
+            CraftedIconsDataGrid.TabIndex = 1;
             // 
             // CraftedFilesWindow
             // 
             AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.FromArgb(179, 205, 224);
-            ClientSize = new Size(1120, 779);
+            ClientSize = new Size(1490, 888);
+            Controls.Add(CraftedIconsDataGrid);
             Controls.Add(CraftedControlPanel);
             FormBorderStyle = FormBorderStyle.Fixed3D;
             MaximizeBox = false;
@@ -53,11 +77,15 @@
             StartPosition = FormStartPosition.CenterParent;
             Text = "Crafted Files Window";
             Load += CraftedFilesWindow_Load;
+            ((System.ComponentModel.ISupportInitialize)destDirManagerBindingSource).EndInit();
+            ((System.ComponentModel.ISupportInitialize)CraftedIconsDataGrid).EndInit();
             ResumeLayout(false);
         }
 
         #endregion
 
         private Panel CraftedControlPanel;
+        private BindingSource destDirManagerBindingSource;
+        private DataGridView CraftedIconsDataGrid;
     }
 }

8b728ed992d98d59ebc71222e7b14a6ae6bfbf08
Timothy Collins <twwc.tech@gmail.com>
2023-11-01 00:23:18 -0600

    removed PngBitmap auto prop

--- backend/IconConverterTool.cs
+++ backend/IconConverterTool.cs
@@ -4,8 +4,6 @@
     {
         public string? PngFilePath { get; set; }
 
-        public Bitmap? PngBitmap { get; private set; }
-
         public int MaxWidth { get; set; } = 128;
 
         public int MinWidth { get; set; } = 64;
@@ -20,14 +18,14 @@
         {
             try
             {
-                PngBitmap = (Bitmap)Image.FromFile(PngFilePath!);
-                if (PngBitmap.Width > MaxWidth || PngBitmap.Width < MinWidth)
+                var pngBitmap = (Bitmap)Image.FromFile(PngFilePath!);
+                if (pngBitmap.Width > MaxWidth || pngBitmap.Width < MinWidth)
                 {
                     MessageBox.Show($"The file must not be greater than {MaxWidth} or less than {MinWidth}", "PNG File Size Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
                     return;
                 }
                 using FileStream stream = File.OpenWrite(SaveDestination!);
-                Icon.FromHandle(PngBitmap.GetHicon()).Save(stream);
+                Icon.FromHandle(pngBitmap.GetHicon()).Save(stream);
             }
             catch (Exception ex)
             {

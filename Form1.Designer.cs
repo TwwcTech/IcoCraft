5f8c1a449cd7758054b553d64511e271e5c660e2
Timothy Collins <twwc.tech@gmail.com>
2023-10-31 22:55:57 -0600

    textboxes removed

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -31,8 +31,6 @@
             ControlPanel = new Panel();
             ViewCraftedButton = new Button();
             CraftButton = new Button();
-            PngPathEntryBox = new TextBox();
-            IconNameEntryBox = new TextBox();
             ControlPanel.SuspendLayout();
             SuspendLayout();
             // 
@@ -42,17 +40,19 @@
             ControlPanel.Controls.Add(ViewCraftedButton);
             ControlPanel.Controls.Add(CraftButton);
             ControlPanel.Dock = DockStyle.Bottom;
-            ControlPanel.Location = new Point(0, 397);
+            ControlPanel.Location = new Point(0, 238);
+            ControlPanel.Margin = new Padding(2, 2, 2, 2);
             ControlPanel.Name = "ControlPanel";
-            ControlPanel.Size = new Size(950, 131);
+            ControlPanel.Size = new Size(665, 79);
             ControlPanel.TabIndex = 0;
             // 
             // ViewCraftedButton
             // 
             ViewCraftedButton.Font = new Font("Century Gothic", 12F, FontStyle.Bold, GraphicsUnit.Point);
-            ViewCraftedButton.Location = new Point(470, 36);
+            ViewCraftedButton.Location = new Point(329, 22);
+            ViewCraftedButton.Margin = new Padding(2, 2, 2, 2);
             ViewCraftedButton.Name = "ViewCraftedButton";
-            ViewCraftedButton.Size = new Size(317, 59);
+            ViewCraftedButton.Size = new Size(222, 35);
             ViewCraftedButton.TabIndex = 1;
             ViewCraftedButton.Text = "VIEW CRAFTED";
             ViewCraftedButton.UseVisualStyleBackColor = true;
@@ -61,43 +61,25 @@
             // CraftButton
             // 
             CraftButton.Font = new Font("Century Gothic", 12F, FontStyle.Bold, GraphicsUnit.Point);
-            CraftButton.Location = new Point(137, 36);
+            CraftButton.Location = new Point(96, 22);
+            CraftButton.Margin = new Padding(2, 2, 2, 2);
             CraftButton.Name = "CraftButton";
-            CraftButton.Size = new Size(315, 59);
+            CraftButton.Size = new Size(220, 35);
             CraftButton.TabIndex = 0;
             CraftButton.Text = "CRAFT";
             CraftButton.UseVisualStyleBackColor = true;
             CraftButton.Click += CraftButton_Click;
             // 
-            // PngPathEntryBox
-            // 
-            PngPathEntryBox.Font = new Font("Century Gothic", 12F, FontStyle.Regular, GraphicsUnit.Point);
-            PngPathEntryBox.Location = new Point(137, 120);
-            PngPathEntryBox.Name = "PngPathEntryBox";
-            PngPathEntryBox.PlaceholderText = "Drag and Drop your PNG file here";
-            PngPathEntryBox.Size = new Size(650, 37);
-            PngPathEntryBox.TabIndex = 1;
-            // 
-            // IconNameEntryBox
-            // 
-            IconNameEntryBox.Font = new Font("Century Gothic", 12F, FontStyle.Regular, GraphicsUnit.Point);
-            IconNameEntryBox.Location = new Point(137, 220);
-            IconNameEntryBox.Name = "IconNameEntryBox";
-            IconNameEntryBox.PlaceholderText = "Enter name for crafted Icon file";
-            IconNameEntryBox.Size = new Size(650, 37);
-            IconNameEntryBox.TabIndex = 2;
-            // 
             // MainFrame
             // 
-            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.FromArgb(179, 205, 224);
-            ClientSize = new Size(950, 528);
-            Controls.Add(IconNameEntryBox);
-            Controls.Add(PngPathEntryBox);
+            ClientSize = new Size(665, 317);
             Controls.Add(ControlPanel);
             FormBorderStyle = FormBorderStyle.Fixed3D;
             HelpButton = true;
+            Margin = new Padding(2, 2, 2, 2);
             MaximizeBox = false;
             Name = "MainFrame";
             StartPosition = FormStartPosition.CenterScreen;
@@ -105,14 +87,11 @@
             Load += MainFrame_Load;
             ControlPanel.ResumeLayout(false);
             ResumeLayout(false);
-            PerformLayout();
         }
 
         #endregion
 
         private Panel ControlPanel;
-        private TextBox PngPathEntryBox;
-        private TextBox IconNameEntryBox;
         private Button CraftButton;
         private Button ViewCraftedButton;
     }
